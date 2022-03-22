eedd4e4e9cd7676cbff6d59aa329a674bd5c5d08
lecheliza <lechoosz@gmail.com>
2022-03-23 00:51:42 +0100

    something that works but is veeery slow

--- fast_partition_refinement.py
+++ fast_partition_refinement.py
@@ -1,57 +1,61 @@
+import time
 from collections import deque
-from libraries import graph_io
-from libraries.graph import *
-from libraries.graph_io import load_graph
+from libraries.graph_io import *
 
 
-def add_to_adequate_color(m: "list", s2: "deque"):
-    m.sort()
-    for color in m:
-        s2.append(color)
+def map_occurrences_with_vertices(vertices_list, color):
+    dictionary = {}
+    for vertex in vertices_list:
+        number_of_occurrences = len([n for n in vertex.neighbours if n.colornum == color])
+        if number_of_occurrences in dictionary:
+            dictionary[number_of_occurrences].append(vertex)
+        else:
+            dictionary[number_of_occurrences] = [vertex]
+    return dictionary
 
 
 def fast_partition_refinement(input_graph: "Graph"):
-    w = deque()
-    colors = []
+    w, colors = deque(), []
     for v in input_graph.vertices:
         v.colornum = v.degree
         if v.colornum not in colors:
             colors.append(v.colornum)
-    add_to_adequate_color(colors, w)
+    colors.sort()
+    for color in colors:
+        w.append(color)
     while w:
-        next_color = max(w) + 1
+        higher_color = max(w) + 1
         a = w.popleft()
-        for color in range(1, next_color):
-            occurrences_to_vertices = {}
-            vertices_with_examined_color = [v for v in input_graph.vertices if v.colornum == color]
-            for vertex in vertices_with_examined_color:
-                index = 0
-                for n in vertex.neighbours:
-                    if n.colornum == a:
-                        index += 1
-                if index not in occurrences_to_vertices.keys():
-                    occurrences_to_vertices[index] = [vertex]
-                else:
-                    occurrences_to_vertices[index].append(vertex)
-            index_of_biggest_occurences = 0
-            number_of_vertices_in_this_index = 0
-            for item in occurrences_to_vertices.items():
-                num_ver = len(item[1])
-                if number_of_vertices_in_this_index < num_ver:
-                    number_of_vertices_in_this_index = num_ver
-                    index_of_biggest_occurences = item[0]
-            occurrences_to_vertices.pop(index_of_biggest_occurences)
-            for ncc in occurrences_to_vertices.values():
-                for v in ncc:
-                    v.colornum = next_color
-                w.append(next_color)
-                next_color += 1
+        c = 0
+        while c < len(colors):
+            color = colors[c]
+            # vertices_with_color = []
+            # for vertex in input_graph.vertices:
+            #     if vertex.colornum == color:
+            #         vertices_with_color.append(vertex)
+            vertices_with_color = [v for v in input_graph.vertices if v.colornum == color]
+            occurrences_to_vertices = map_occurrences_with_vertices(vertices_with_color, a)
+            if occurrences_to_vertices:
+                needed_index = max(occurrences_to_vertices, key=lambda x: len(occurrences_to_vertices[x]))
+                occurrences_to_vertices.pop(needed_index)
+                for vertices_for_new_color in occurrences_to_vertices.values():
+                    for vertex in vertices_for_new_color:
+                        vertex.colornum = higher_color
+                    w.append(higher_color)
+                    colors.append(higher_color)
+                    higher_color += 1
+            c += 1
     with open('../partition_refinement/test_files/done1.dot', 'w') as f:
-        graph_io.write_dot(input_graph, f)
+        write_dot(input_graph, f)
     return
 
 
-with open('../partition_refinement/test_files/colorref_smallexample_4_7.grl') as f:
+with open('../partition_refinement/test_files/SignOffColRefBackup5.grl') as f:
     graphs = load_graph(f, read_list=True)
 
-fast_partition_refinement(graphs[0][0])
+start_time = time.time()
+huge_graph = Graph(False)
+# for g in graphs[0]:
+#     huge_graph = huge_graph + g
+fast_partition_refinement(graphs[0][0] + graphs[0][2])
+print("--- %s seconds ---" % (time.time() - start_time))
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from color_refinement import *
+from fast_partition_refinement import fast_partition_refinement
 from libraries.graph import *
 from libraries.graph_io import *
 import time
 
 start_time = time.time()
-with open('../partition_refinement/test_files/colorref_smallexample_4_7.grl') as f:
+with open('../partition_refinement/test_files/SignOffColRefBackup5.grl') as f:
     graphs = load_graph(f, read_list=True)
 
-# huge_graph = graph.Graph(False)
-# for g in graphs[0]:
-#     huge_graph = huge_graph + g
-n = color_refinement(graphs[0])
+huge_graph = graph.Graph(False)
+for g in graphs[0]:
+    huge_graph = huge_graph + g
+# n = color_refinement(huge_graph)
+fast_partition_refinement(huge_graph)
 print("--- %s seconds ---" % (time.time() - start_time))
