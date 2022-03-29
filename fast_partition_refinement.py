ba621c8a45519b60be973a51573e05ead958e964
lecheliza <lechoosz@gmail.com>
2022-03-29 14:06:57 +0200

    faster faster fpr

--- fast_partition_refinement.py
+++ fast_partition_refinement.py
@@ -22,42 +22,42 @@ def fast_partition_refinement(input_graph: "Graph"):
         vertex.colornum = new_color
         if new_color not in w:
             w.append(new_color)
-        colors.add(new_color)
         if vertex.colornum not in colors_with_vertices:
             colors_with_vertices[new_color] = [vertex]
         else:
             colors_with_vertices[new_color].append(vertex)
-    colors_to_add = set()
+    c_t_a = dict()
     while w:
-        colors_to_add.clear()
+        c_t_a.clear()
         higher_color = max(w) + 1
         a = w.popleft()
-        for color in colors:
+        for color in colors_with_vertices.keys():
             occurrences_to_vertices = map_occurrences_with_vertices(colors_with_vertices[color], a)
-            if occurrences_to_vertices:
+            if len(occurrences_to_vertices) < 1:
+                continue
+            else:
                 needed_index = max(occurrences_to_vertices, key=lambda x: len(occurrences_to_vertices[x]))
                 occurrences_to_vertices.pop(needed_index)
                 for vertices_for_new_color in occurrences_to_vertices.values():
-                    colors_with_vertices[higher_color] = []
+                    c_t_a[higher_color] = []
                     for vertex in vertices_for_new_color:
                         colors_with_vertices[vertex.colornum].remove(vertex)
                         vertex.colornum = higher_color
-                        colors_with_vertices[higher_color].append(vertex)
+                        c_t_a[higher_color].append(vertex)
                     w.append(higher_color)
-                    colors_to_add.add(higher_color)
                     higher_color += 1
-        colors.update(colors_to_add)
+        colors_with_vertices.update(c_t_a)
     with open('../partition_refinement/test_files/done1.dot', 'w') as f:
         write_dot(input_graph, f)
     return
 
 
-with open('../partition_refinement/test_files/SignOffColRefBackup5.grl') as f:
+with open('../partition_refinement/test_files/colorref_smallexample_4_7.grl') as f:
     graphs = load_graph(f, read_list=True)
 
 start_time = time.time()
-# huge_graph = Graph(False)
-# for g in graphs[0]:
-#     huge_graph = huge_graph + g
-fast_partition_refinement(graphs[0][0] + graphs[0][1])
+huge_graph = Graph(False)
+for g in graphs[0]:
+    huge_graph = huge_graph + g
+fast_partition_refinement(huge_graph)
 print("--- %s seconds ---" % (time.time() - start_time))
